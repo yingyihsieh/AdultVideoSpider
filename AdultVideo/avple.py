337e49966b2fb517dd20d20b306555db33cd27a6
andyhsieh <ady9430@gmail.com>
2022-06-07 18:03:26 +0800

    avple test ver3

--- AdultVideo/avple.py
+++ AdultVideo/avple.py
@@ -166,6 +166,7 @@ def demo(url):
     hash_tags = [i.decode() for i in hash_tags]
     print(hash_tags)
 if __name__ == '__main__':
-    demo('your url')
+    # git demo2
+    # git demo1
     avs = AvpleSpider()
     avs.run()
